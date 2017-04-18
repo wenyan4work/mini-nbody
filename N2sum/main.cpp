e1a794c2e1ce3c2bd4334469ccaaf7a55b6b3c8e
Wen Yan <wenyan4work@gmail.com>
2017-04-18 09:56:25 -0400

    initial fork

---
+++ N2sum/main.cpp
@@ -0,0 +1,18 @@
+#include <omp.h>
+#include <vector>
+#include <cstdio>
+
+
+void n2CPU(){
+
+}
+
+
+int main(int argc, char** argv){
+    int nSrc=32768;
+    int nTrg=nSrc;
+    
+    if (argc > 1) nBodies = atoi(argv[1]);
+    
+}
+
